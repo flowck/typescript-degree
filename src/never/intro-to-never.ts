0f800b71c5dea8c7328620f366cb0d0bcd1124d7
Firmino Changani <firmino.changani@mercedes-benz.io>
2020-07-26 23:30:07 +0100

    feat: Intro to never

--- src/never/intro-to-never.ts
+++ src/never/intro-to-never.ts
@@ -7,3 +7,18 @@
 function handleHttpErrors(error, code): never {
   throw { code, error }
 }
+
+// Source https://stackoverflow.com/questions/42291811/use-of-never-keyword-in-typescript
+type SomeThing<T> = T extends null | undefined ? never : T;
+
+const Y: SomeThing<number> = 5;
+
+type First<T> = T extends [any, any] ? T[0] : never;
+type Second<T> = T extends [any, any] ? T[1] : never;
+
+const x = [122, 3, 4, 212, 4];
+
+interface Z {
+  
+}
+const y: First<> = x[0];
